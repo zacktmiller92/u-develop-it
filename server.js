176488c8e7e21524d7ad1547d80e0cc74be30d1d
zacktmiller92 <zacktmiller92@gmail.com>
2021-08-29 17:42:25 -0400

    added query from candidates db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'zack',
+        // Your MySQL password
+        password: 'zack',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.get('/', (req, res) => {
     res.json({
@@ -14,10 +27,15 @@ app.get('/', (req, res) => {
     });
 });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    // console.log(rows);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
