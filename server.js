101acc06d229a2253c34ab7e84c8a446da16a09e
zacktmiller92 <zacktmiller92@gmail.com>
2021-08-29 17:59:09 -0400

    added delete and insert query functions

--- server.js
+++ server.js
@@ -31,11 +31,41 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
     // console.log(rows);
 });
 
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    // console.log(row);
+});
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+
+
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
